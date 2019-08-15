35161feda0baddf838915caf17763aea1d18438d
Wesley <wing3000@gmail.com>
2019-08-14 23:26:13 -0700

    Store IP geolocation data

--- src/app/bill/bill.component.ts
+++ src/app/bill/bill.component.ts
@@ -140,19 +140,32 @@ export class BillComponent implements OnInit {
     this.http.get('https://jsonip.com').subscribe(
       (res) => {
         ip = res['ip'];
-        this.db.collection('bills').doc(this.billId).collection('paid').doc(ip).set({
-          ip,
-          amount: this.totalStr,
-          items: this.checkedItems,
-          timestamp: new Date(),
-        })
-        .then(() => {
-          console.log(ip + ' marked as paid');
-          window.location.href = this.paypalBaseUrl;
-        })
-        .catch((err) => {
-          console.error('IP Marked payment error ' + ip, err);
-        });
+        this.http.get('http://ip-api.com/json/' + ip).subscribe(
+          (res) => {
+            this.db.collection('bills').doc(this.billId).collection('paid').doc(ip).set({
+              ip,
+              city: res['city'],
+              region: res['region'],
+              regionName: res['regionName'],
+              isp: res['isp'],
+              lat: res['lat'],
+              lon: res['lon'],
+              org: res['org'],
+              zip: res['zip'],
+              country: res['country'],
+              amount: this.totalStr,
+              items: this.checkedItems,
+              timestamp: new Date(),
+            })
+            .then(() => {
+              console.log(ip + ' marked as paid');
+              window.location.href = this.paypalBaseUrl;
+            })
+            .catch((err) => {
+              console.error('IP Marked payment error ' + ip, err);
+            });
+          }
+        );
       },
       (err) => {
         console.error('IP Error ' + ip, err);
